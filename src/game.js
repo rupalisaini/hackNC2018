31424237055e6b56f514cd27dd047e860e9bc30e
Rupali Saini <rupali@rupalis-air.wireless-1x.unc.edu>
2018-10-06 15:21:36 -0400

    boom

--- src/game.js
+++ src/game.js
@@ -2,7 +2,26 @@
 exports.__esModule = true;
 var Game = /** @class */ (function () {
     function Game() {
+        this.banned = [];
     }
+    Game.prototype.banWord = function (a) {
+        this.banned.push(a);
+    };
+    Game.prototype.banCheck = function (a) {
+        var n = -1;
+        for (var i = 0; i < this.banned.length; i++) {
+            if (n === -1) {
+                var r = new RegExp(this.banned[i], "i");
+                n = a.search(r);
+            }
+        }
+        return n;
+    };
     return Game;
 }());
 exports.Game = Game;
+var a = new Game();
+a.banWord("lolx");
+a.banWord("a");
+console.log(a.banned);
+console.log(a.banCheck("i'm a little bitcho"));
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,30 @@
 export class Game {
     
+    banned: string[] = [];
+
+    constructor(){
+    }
+
+    banWord(a: string) {
+        this.banned.push(a);
+    }
+
+    banCheck(a: string){
+
+        let n: number = -1;
+
+        for (let i: number = 0; i < this.banned.length; i++){
+            if (n === -1){
+                let r = new RegExp(this.banned[i], "i");
+                n = a.search(r);             
+            }
+        }
+        return n;
+    }
 }
+
+let a: Game = new Game();
+a.banWord("lolx");
+a.banWord("a");
+console.log(a.banned);
+console.log(a.banCheck("i'm a little bitcho"));
