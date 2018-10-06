8541fcfb083e291f55b29f8c79edde2f4af6a2f1
Nivrad00 <darvinheo@gmail.com>
2018-10-06 15:06:05 -0400

    start and ban commands

---
+++ src/config.js
@@ -0,0 +1,3 @@
+"use strict";
+exports.__esModule = true;
+exports.token = "NDk4MTYwMjQ4MDg4MTY2NDAw.Dpprdw.c1MTXCBeJU8yUT3Z5-JDTmFrlso";
--- src/wwtmtd.ts
+++ src/wwtmtd.ts
@@ -4,7 +4,7 @@ import * as config from './config';
 import { Game } from './game';
 
 const prefix = 'tator';
-let games: Map<number, Game>;
+let games: Map<string, Game> = new Map<string, Game>();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,19 +12,50 @@ client.on('ready', () => {
 
 client.on('message', message => {
     let exec: RegExpExecArray = new RegExp('^' + prefix + ' (.*)$').exec(message.content);
+    let channel: Discord.TextChannel = message.channel as Discord.TextChannel;
+    let game = games.get(channel.id);
+
+    // check for commands
     if (exec != null)
-        handleCommand(exec[1].trim());
+        handleCommand(exec[1].trim(), channel, game);
+
+    // then check for banned words
+    else if (game) {
+        if (game.banCheck(message.content))
+            channel.send('BITCH');
+    }
+
     console.log(message.content);
 });
 
-function handleCommand (input: string): void {
-    let args: string[] = input.split(' ');
-    let command: string = args.shift();
-    console.log('handling command: ' + command);
+function handleCommand (input: string, channel: Discord.TextChannel, game: Game): void {
+    let inputArr: string[] = input.split(' ');
+    let command: string = inputArr.shift();
+    let arg: string = inputArr.join(' ');
+
+    // check to make sure game exists
+
+    if (game == null) {
+        if (command == 'start') {
+            games.set(channel.id, new Game())
+            channel.send('Started game.');
+        }
+        else channel.send('There\'s no game running in this channel.');
+        return;
+    }
+
+    // if game exists...
 
     switch (command) {
-        case 'start':
+        case 'ban':
+            game.banWord(arg);
+            channel.send('Banned word: ' + arg);
             break;
+
+        case 'start':
+            channel.send('Game already exists in this channel.');
+            break;
+
         case 'join':
             break;
         case 'leave':
